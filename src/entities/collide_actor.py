9a8ddacefa03852d2768c78685208d9c8eac74cd
Sid-26 <siddhantdas262@gmail.com>
2026-02-10 01:56:51 -0500

    added entities from original game as seperate entities

--- src/entities/collide_actor.py
+++ src/entities/collide_actor.py
@@ -1,4 +1,7 @@
 import pygame, pgzero
+from consts import ANCHOR_CENTRE, GRID_BLOCK_SIZE
+
+# to do figure out block function
 
 class CollideActor(Actor):
     def __init__(self, pos, anchor=ANCHOR_CENTRE):
@@ -15,19 +18,7 @@ class CollideActor(Actor):
                 # Collided with edge of level
                 return True
 
-            # Normally you don't need brackets surrounding the condition for an if statement (unlike many other
-            # languages), but in the case where the condition is split into multiple lines, using brackets removes
-            # the need to use the \ symbol at the end of each line.
-            # The code below checks to see if we're position we're trying to move into overlaps with a block. We only
-            # need to check the direction we're actually moving in. So first, we check to see if we're moving down
-            # (dy > 0). If that's the case, we then check to see if the proposed new y coordinate is a multiple of
-            # GRID_BLOCK_SIZE. If it is, that means we're directly on top of a place where a block might be. If that's
-            # also true, we then check to see if there is actually a block at the given position. If there's a block
-            # there, we return True and don't update the object to the new position.
-            # For movement to the right, it's the same except we check to ensure that the new x coordinate is a multiple
-            # of GRID_BLOCK_SIZE. For moving left, we check to see if the new x coordinate is the last (right-most)
-            # pixel of a grid block.
-            # Note that we don't check for collisions when the player is moving up.
+
             if ((dy > 0 and new_y % GRID_BLOCK_SIZE == 0 or
                  dx > 0 and new_x % GRID_BLOCK_SIZE == 0 or
                  dx < 0 and new_x % GRID_BLOCK_SIZE == GRID_BLOCK_SIZE-1)
@@ -39,3 +30,5 @@ class CollideActor(Actor):
 
         # Didn't collide with block or edge of level
         return False
+    
+    
---
+++ src/entities/pop.py
@@ -0,0 +1,10 @@
+class Pop(Actor):
+    def __init__(self, pos, type):
+        super().__init__("blank", pos)
+
+        self.type = type
+        self.timer = -1
+
+    def update(self):
+        self.timer += 1
+        self.image = "pop" + str(self.type) + str(self.timer // 2)
