21994286dbdbd2baaa164458320c55971a1e4871
Sid-26 <siddhantdas262@gmail.com>
2026-02-09 23:27:13 -0500

    in the process of moving code for game logic

--- src/game.py
+++ src/game.py
@@ -1,8 +1,21 @@
+from random import randint, shuffle
+
 class Game:
-    pass
+    def block(self, x,y):
+        # Is there a level grid block at these coordinates?
+        grid_x = (x - LEVEL_X_OFFSET) // GRID_BLOCK_SIZE
+        grid_y = y // GRID_BLOCK_SIZE
+
+        if grid_y > 0 and grid_y < NUM_ROWS:
+            row = self.grid[grid_y]
+            return grid_x >= 0 and grid_x < NUM_COLUMNS and len(row) > 0 and row[grid_x] != " "
+        else:
+            return False
 
 class Player:
     pass
 
-def draw_status():
-    pass
+# utils
+def sign(x):
+    # Returns -1 or 1 depending on whether number is positive or negative
+    return -1 if x < 0 else 1
