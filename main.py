ecbf2070c0a58a235f3b7e96d77509289621a4cd
Sid-26 <siddhantdas262@gmail.com>
2026-02-09 22:42:27 -0500

    added entry point for app to pgzero

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from src.app import App
+from src.game import Game
+from src.screens.menu import MenuScreen
+import pgzero, pgzrun
 
 if __name__ == "__main__":
-    game = App()
-    game.run()
+    app = App(MenuScreen(Game()))
+
+    def update():
+        app.update()
+
+    def draw():
+        app.draw()
+
+    pgzrun.go()
