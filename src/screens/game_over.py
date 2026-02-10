c129f2530bbfaa309876a171cd963d458c1baa25
Sid-26 <siddhantdas262@gmail.com>
2026-02-09 22:45:48 -0500

    added GameOverScreen

--- src/screens/game_over.py
+++ src/screens/game_over.py
@@ -1,4 +1,17 @@
 from screen import Screen
+from menu import MenuScreen
+from game import Game, draw_status
+import pgzero
 
 class GameOverScreen(Screen):
-    pass
+    def _init_(self, game):
+        self.game = game
+    
+    def update(self, app, input_state):
+        if input_state.fire_pressed:
+            app.change_screen(MenuScreen(Game()))
+    
+    def draw(self):
+        self.game.draw()
+        draw_status()
+        screen.blit("over", (0, 0))
