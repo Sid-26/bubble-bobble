b3803e9b9fb61f2e36395327e67471aef9870905
Sid-26 <siddhantdas262@gmail.com>
2026-02-09 20:22:06 -0500

    finished app class to handle screens

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
 import pygame, pgzero, pgzrun, sys
 
 class App:
-    def __init__(self) -> None:
-        pass
-    def run(self):
-        pass
+    def __init__(self, screen) -> None:
+        self.current_screen = screen
+
+    def change_screen(self, next_screen):
+        self.current_screen = next_screen
+    
+    def update(self):
+        self.current_screen.update(self)
+    
+    def draw(self):
+        self.current_screen.draw()
