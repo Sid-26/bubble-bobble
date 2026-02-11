cbe4c7c03b1cb4c6e3f74f59ab7f9d656dd2e954
Sid-26 <siddhantdas262@gmail.com>
2026-02-10 22:01:36 -0500

    fixed critical bug that was crashing pgzero on run

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+print("checking if imports")
 from src.app import App
 from src.game import Game
 from src.screens.menu import MenuScreen
 import pgzero, pgzrun
 
-if __name__ == "__main__":
-    app = App(MenuScreen(Game()))
+print("just before main")
+# if __name__ == "__main__":
+print("trying to run main")
+app = App(MenuScreen(Game()))
 
-    def update():
-        app.update()
+def update():
+    app.update()
 
-    def draw():
-        app.draw()
+def draw():
+    app.draw()
 
-    try:
-        pgzrun.go()
-    except Exception as e:
-        print(f"Fatal Error: {e}")
-        import traceback
-        traceback.print_exc()
+try:
+    pgzrun.go()
+    print("finished running")
+except Exception as e:
+    print(f"Fatal Error: {e}")
+    import traceback
+    traceback.print_exc()
 
 # to do for future sid, please implement the player logic here with input state (COMPLETED)
 # further to do finish task C (Pause menu) (COMPLETED)
